baa0d2cc9b96d8fae4736204cae8a2b53f7610b7
Jon Swanson <jswan2@uw.edu>
2015-05-05 15:37:07 -0700

    got front end working

---
+++ js/uw.disqus-terms.js
@@ -0,0 +1,23 @@
+UW = UW || {
+  elements : {},
+}
+
+_.extend(UW.elements, {
+  disqus : '#disqus_thread'
+});
+
+(function() {
+  var uw_init = UW.initialize;
+  UW.initialize = extended_init; 
+  function extended_init ($) {
+    uw_init($);
+    UW.disqus_terms = _.map( $( UW.elements.disqus ),    function( element ) { return new UW.Disqus_Terms({ el : element }) } )
+  }
+})();
+
+UW.Disqus_Terms = function(args){
+  this.template = '<p id="disqus-terms">' + uw.disqus_tos_text + '</p>';
+  this.$disqus_div = $(args.el);
+  $(this.template).insertBefore(this.$disqus_div);
+  //this.$disqus_div.prepend(this.template);
+}
